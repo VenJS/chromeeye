d86fa2b7e59eaa740cfd591ebeb01a4ac8fb90c2
Ventsislav Kiskinov <venci34@gmail.com>
2024-11-29 14:41:08 +0200

    feat: add portfolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,17 @@
 "use client";
-
-// import Loader from "@/components/Loader/Loader";
-import dynamic from "next/dynamic";
 import styles from "./page.module.scss";
-import React, { Suspense } from "react";
-
-const Header = dynamic(() => import("@/components/Header/Header"), {
-  ssr: false,
-});
-const Hero = dynamic(() => import("@/components/Hero/Hero"), {
-  ssr: false,
-});
-const Services = dynamic(() => import("@/components/Services/Services"), {
-  ssr: false,
-});
+import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
+import Services from "@/components/Services/Services";
+import Portfolio from "@/components/Portfolio/Portfolio";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Suspense fallback={<div>Loading...</div>}>
         <Header />
         <Hero />
         <Services />
-      </Suspense>
+        <Portfolio />
     </div>
   );
 }
---
+++ src/components/Portfolio/Portfolio.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./Portfolio.module.scss";
+import { Typography } from "../Typography/Typography";
+import { useFetchData } from "@/services/useFetchData";
+import Button from "../Button/Button";
+
+const Portfolio = () => {
+  const { portfolio } = useFetchData();
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.introduction}>
+        <Typography variant="medium-title">Our work</Typography>
+        <Typography variant="description">{`Our ${portfolio?.description}`}</Typography>
+      </div>
+      <div className={styles.products}>
+        {portfolio?.items.map((item) => {
+          return (
+            <div key={item.title} className={styles.productSection}>
+              <div className={styles.description}>
+                <Typography variant="medium-title">{item.title}</Typography>
+                <Typography variant="description">
+                  {item.description}
+                </Typography>
+                <Button href={item.buttonUrl} label={item.buttonLabel} />
+              </div>
+              <img className={styles.image} src={item.image} alt="" />
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
